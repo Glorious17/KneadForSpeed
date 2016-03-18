398bf328a5f4bb7bf73f52c5ac1776f17affd0f7
Noki <ndinh@hs-bremen.de>
2016-03-18 12:01:41 +0100

    - Touch detection

---
+++ KneadForSpeed/Assets/scripts/Touchscript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touchscript : MonoBehaviour {
+
+	void Update () 
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch mytouch = Input.GetTouch (0);
+
+			//Touch[] mytouches = Input.touches;
+
+			if (mytouch.phase == TouchPhase.Began) {
+				if (mytouch.position.y <= Screen.height / 2) {
+					if (mytouch.position.x <= Screen.width / 2) {
+						Debug.Log ("Unten Links");
+					}
+					if (mytouch.position.x > Screen.width / 2) {
+						Debug.Log ("Unten Rechts");
+					}
+				}
+
+				if (mytouch.position.y > Screen.height / 2) {
+					if (mytouch.position.x <= Screen.width / 2) {
+						Debug.Log ("Oben Links");
+					}
+					if (mytouch.position.x > Screen.width / 2) {
+						Debug.Log ("Oben Rechts");
+					}
+				}
+			}
+		}
+	}
+}
