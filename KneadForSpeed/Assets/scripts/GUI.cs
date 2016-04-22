99f0fcd0dce6a27da65495aa739961f08c18257c
xxdavincixx <lbartling@stud.hs-bremen.de>
2016-04-22 12:55:03 +0200

    Revert "Score-Berechnung verbessert"
    This reverts commit 09a8aa6420ce4bdbb80de4ec7238d42669ea5bb5.

--- KneadForSpeed/Assets/scripts/GUI.cs
+++ KneadForSpeed/Assets/scripts/GUI.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class GUI : MonoBehaviour {
 
 	public static int score;
-	private static int hits = 0;
-	private static int multiplicity = 0;
+	private static int multiplicity=0;
 
 	void OnGUI() {
 		GUILayout.BeginArea(new Rect(Screen.width - 105, 5, 100, 80));
@@ -21,19 +20,23 @@ public class GUI : MonoBehaviour {
 	}
 
 	public static void good (){
-		
-		hits++;
-		if (hits % 3 == 0)
-			multiplicity++;
-		score += multiplicity;
+
+		multiplicity++;
+		if (score == 0) {
+			score++;
+		}else
+			score= score+multiplicity*score;
+
 	}
 
 	public static void notGood (){
-		score += multiplicity;
+		if (multiplicity > 0)
+			score = score + multiplicity*score;
+		else
+			score++;
 	}
 
 	public static void bad (){
-		multiplicity = 1;
-		hits = 0;
+		multiplicity = 0;
 	}
 }
