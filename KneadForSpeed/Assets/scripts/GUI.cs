9c0d7e2562855f31b4783b7e6d8bb2d2ad0bceb4
xxdavincixx <lbartling@stud.hs-bremen.de>
2016-04-22 11:14:20 +0200

    Score
    Score und Multiplikator wurden eingefügt benötigen aber Überarbeitung
    bei der Berechnung

---
+++ KneadForSpeed/Assets/scripts/GUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour {
+
+	public static int score;
+	private static int multiplicity=0;
+
+	void OnGUI() {
+		GUILayout.BeginArea(new Rect(Screen.width - 105, 5, 100, 80));
+
+		GUILayout.Label("Multiplikator: "+ multiplicity);
+		GUILayout.EndArea();
+
+		GUILayout.BeginArea(new Rect(Screen.width -105, 38, 100, 80));
+
+		GUILayout.Label("Score: " + score);
+		GUILayout.EndArea();
+
+	}
+
+	public static void good (){
+
+		multiplicity++;
+		if (score == 0) {
+			score++;
+		}else
+			score= score+multiplicity*score;
+
+	}
+
+	public static void notGood (){
+		if (multiplicity > 0)
+			score = score + multiplicity*score;
+		else
+			score++;
+	}
+
+	public static void bad (){
+		multiplicity = 0;
+	}
+}
